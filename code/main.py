27baeb5c748bfc7325af0a3eda0242f3a68a7786
Unknown <jarivdheijden@gmail.com>
2019-05-13 14:24:05 +0200

    push for structure and main.py
    this is just a small push so the file structure won't be removed when switching branches

---
+++ code/main.py
@@ -0,0 +1,7 @@
+import random
+
+rpsTupleJhei = ("Rock", "Paper", "Scissors")
+
+print("Rock Paper Code", "This project was made by Jari van der Heijden, Thijs van Kessel and Amin Khachiche", "The function of this project", sep="\n")
+
+input()
