07cb9aa38c563fc75d0c16b9e66fdf6c5139ae13
Unknown <jarivdheijden@gmail.com>
2019-05-13 15:36:08 +0200

    text rps
    this is the start of the text based rps. the foundation

--- code/main.py
+++ code/main.py
@@ -1,7 +1,26 @@
-import random
+import random, sys, time
 
 rpsTupleJhei = ("Rock", "Paper", "Scissors")
+aiPickJhei = ""
+
 
 print("Rock Paper Code", "This project was made by Jari van der Heijden, Thijs van Kessel and Amin Khachiche", "The function of this project", sep="\n")
 
-input()
+userPickJhei = input("Pick: Rock, Paper or scissors?\n")
+
+if "k" in userPickJhei.lower():
+    userPickJhei = "Rock"
+elif "p" in userPickJhei.lower():
+    userPickJhei = "Paper"
+elif "s" in userPickJhei.lower():
+    userPickJhei = "Scissors"
+else:
+    sys.exit("FATAL ERROR CLOSING NOW!")
+
+aiPickJhei = random.choice(rpsTupleJhei)
+print(userPickJhei)
+print(aiPickJhei)
+
+if aiPickJhei == userPickJhei:
+    print("Tie!")
+if aiPickJhei == "Rock"
