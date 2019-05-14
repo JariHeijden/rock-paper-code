7920115f14177f6437866cd26ff2d96eb0868130
Unknown <jarivdheijden@gmail.com>
2019-05-14 14:19:14 +0200

    main push
    pushin code so amin can download opencv on my laptop

--- code/main.py
+++ code/main.py
@@ -18,9 +18,22 @@ else:
     sys.exit("FATAL ERROR CLOSING NOW!")
 
 aiPickJhei = random.choice(rpsTupleJhei)
-print(userPickJhei)
 print(aiPickJhei)
 
 if aiPickJhei == userPickJhei:
-    print("Tie!")
-if aiPickJhei == "Rock"
+    print("You both played", aiPickJhei, "Tie!")
+if aiPickJhei == "Rock":
+    if userPickJhei == "Paper":
+        print("You covered the rock using paper \nYou won!")
+    elif userPickJhei == "Scissors":
+        print("Your scissors got destroyed with a rock...\nYou lost")
+if aiPickJhei == "Paper":
+    if userPickJhei == "Scissors":
+        ("You've snipped his paper right in half!\nYou won!")
+    elif userPickJhei == "Rock":
+        ("Your rock didn't have affect on his paper...\nYou lost")
+if aiPickJhei == "Scissors":
+    if userPickJhei == "Rock":
+        print("You obliterated his scissors\nYou won!")
+    elif userPickJhei == "Paper":
+        print("Your paper got cut in half...\nYou lost")
